770a8d710a06be8606faa2ea715d698d7cfb5b1a
wang <wang0201@live.com>
2015-01-30 11:57:07 +0800

    Add the forth Hello World~

--- ConsoleAppTestUsingGit/Program.cs
+++ ConsoleAppTestUsingGit/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleAppTestUsingGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World 2!!");
             Console.WriteLine("Hello World 3!!!");
+            Console.WriteLine("Hello World 4!!!!");
             Console.Read();
         }
     }
