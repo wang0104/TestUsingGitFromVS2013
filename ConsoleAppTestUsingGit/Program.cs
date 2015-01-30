93748e565a2a91eeae9b7bbf32421bef60dc4691
wang <wang0201@live.com>
2015-01-30 11:48:04 +0800

    Add the third Hello World~

--- ConsoleAppTestUsingGit/Program.cs
+++ ConsoleAppTestUsingGit/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleAppTestUsingGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World 2!!");
+            Console.WriteLine("Hello World 3!!!");
             Console.Read();
         }
     }
